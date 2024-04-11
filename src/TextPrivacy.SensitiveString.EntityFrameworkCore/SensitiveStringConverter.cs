7f647cd848abb2ba191d044d6810d3cfb7c1b1d2
Mariusz Schimke <mariusz.schimke@gmail.com>
2024-04-11 22:26:47 +0200

    Remove redundant assemblies copied from the source repository

--- src/TextPrivacy.SensitiveString.EntityFrameworkCore/SensitiveStringConverter.cs
+++ src/TextPrivacy.SensitiveString.EntityFrameworkCore/SensitiveStringConverter.cs
@@ -2,6 +2,9 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace TextPrivacy.SensitiveString.EntityFrameworkCore;
 
+/// <summary>
+///     Handles <see cref="SensitiveString" /> conversion to and from string.
+/// </summary>
 public class SensitiveStringConverter : ValueConverter<SensitiveString, string>
 {
     public SensitiveStringConverter()
